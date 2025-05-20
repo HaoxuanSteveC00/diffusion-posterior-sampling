7595e1756c7dd161357559e37d9dc1930e9afcf0
HaoxuanSteveC00 <56577120+HaoxuanSteveC00@users.noreply.github.com>
2025-05-20 04:27:44 -0700

    Add files via upload

--- scripts/run_sampling.sh
+++ scripts/run_sampling.sh
@@ -3,9 +3,66 @@
 # $1: task
 # $2: gpu number
 
+
+# FFHQ
+
 python3 sample_condition.py \
     --model_config=configs/model_config.yaml \
     --diffusion_config=configs/diffusion_config.yaml \
-    --task_config=configs/$1_config.yaml \
-    --gpu=$2 \
-    --save_dir=$3;
+    --task_config=configs/gaussian_deblur_config.yaml \
+    --gpu=0 \
+    --save_dir=results/;
+
+python3 sample_condition.py \
+    --model_config=configs/model_config.yaml \
+    --diffusion_config=configs/diffusion_config.yaml \
+    --task_config=configs/motion_deblur_config.yaml \
+    --gpu=0 \
+    --save_dir=results/;
+
+python3 sample_condition.py \
+    --model_config=configs/model_config.yaml \
+    --diffusion_config=configs/diffusion_config.yaml \
+    --task_config=configs/super_resolution_config.yaml \
+    --gpu=0 \
+    --save_dir=results/;
+
+python3 sample_condition.py \
+    --model_config=configs/model_config.yaml \
+    --diffusion_config=configs/diffusion_config.yaml \
+    --task_config=configs/inpainting_config.yaml \
+    --gpu=0 \
+    --save_dir=results/;   
+
+
+# ImageNet
+
+python3 sample_condition.py \
+    --model_config=configs/model_imagenet_config.yaml \
+    --diffusion_config=configs/diffusion_config.yaml \
+    --task_config=configs/gaussian_deblur_imagenet_config.yaml \
+    --gpu=0 \
+    --save_dir=results/; 
+
+python3 sample_condition.py \
+    --model_config=configs/model_imagenet_config.yaml \
+    --diffusion_config=configs/diffusion_config.yaml \
+    --task_config=configs/motion_deblur_imagenet_config.yaml \
+    --gpu=0 \
+    --save_dir=results/;
+
+python3 sample_condition.py \
+    --model_config=configs/model_imagenet_config.yaml \
+    --diffusion_config=configs/diffusion_config.yaml \
+    --task_config=configs/super_resolution_imagenet_config.yaml \
+    --gpu=0 \
+    --save_dir=results/;
+
+python3 sample_condition.py \
+    --model_config=configs/model_imagenet_config.yaml \
+    --diffusion_config=configs/diffusion_config.yaml \
+    --task_config=configs/inpainting_imagenet_config.yaml \
+    --gpu=0 \
+    --save_dir=results/;        
+
+
